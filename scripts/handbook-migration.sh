b5b26363ad885985b5266693cadb33493c26316b
Jamie Maynard <12185839-jamiemaynard@users.noreply.gitlab.com>
2023-03-28 13:30:13 +0100

    Quick fix to migration script

--- scripts/handbook-migration.sh
+++ scripts/handbook-migration.sh
@@ -242,10 +242,7 @@ cat << EOF > $DIRECTORY_TO_SPLIT/README.md
 # $TITLE has been migrated
 
 This handbook content has been migrated to the new handbook site and as such this directory
-has been locked from further changes.  If you are looking for this content in the reposity
-you can find it at:
-
-https://gitlab.com/gitlab-com/content-sites/handbook/
+has been locked from further changes.
 
 If you need help or assitance with this please reach out to @jamiemaynard (Develop/Handbooks) or
 @marshall007 (DRI Content Sites).  Alternatively ask your questions on slack in [#handbook](https://gitlab.slack.com/archives/C81PT2ALD)
