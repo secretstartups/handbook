b16b0ec1b4c9c7c3a1db386c0075317d1e007090
Balasankar 'Balu' C <balasankar@gitlab.com>
2024-06-17 18:59:19 +0000

    Do not attempt to lint deleted files by Vale

--- scripts/markdownlint.sh
+++ scripts/markdownlint.sh
@@ -3,7 +3,7 @@ if ! [ -f markdownlint-cli2-codequality.json ]; then
 fi
 
 BRANCH_POINT=$(git merge-base origin/$CI_MERGE_REQUEST_TARGET_BRANCH_NAME origin/$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME)
-MODIFIED_MD_FILES=$(git diff --name-only $BRANCH_POINT origin/$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME | grep '\.md$')
+MODIFIED_MD_FILES=$(git diff --name-only --diff-filter=d $BRANCH_POINT origin/$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME | grep '\.md$')
 
 if [ -n "$MODIFIED_MD_FILES" ]; then
   markdownlint-cli2 $MODIFIED_MD_FILES
